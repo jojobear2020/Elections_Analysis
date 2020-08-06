4fdcf70e3c399896d235eeb772992414436cf3f8
loddo115 <larysaoddo@gmail.com>
2020-08-06 18:58:16 -0400

    Adding Elections results modified to push result to txt

--- PyPoll.py
+++ PyPoll.py
@@ -3,19 +3,23 @@ print("Elections! Elections! Elections!")
 # Add our dependencies.
 import csv
 import os
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("Resources", "election_analysis.txt")
 # Initialize a total vote counter.
+
 total_votes = 0
 # Candidate options and candidate votes.
 candidate_options = []
 candidate_votes = {}
+
 # Track the winning candidate, vote count, and percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -46,6 +50,7 @@ with open(file_to_save, "w") as txt_file:
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n")
     print(election_results, end="")
+    
     # Save the final vote count to the text file.
     txt_file.write(election_results)
     for candidate_name in candidate_votes:
@@ -64,6 +69,7 @@ with open(file_to_save, "w") as txt_file:
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
+    
     # Print the winning candidate's results to the terminal.
     winning_candidate_summary = (
         f"-------------------------\n"
@@ -72,6 +78,7 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
+    
     # Save the winning candidate's results to the text file.
     open("Resources/election_analysis.txt","w")
     txt_file.write(winning_candidate_summary)
